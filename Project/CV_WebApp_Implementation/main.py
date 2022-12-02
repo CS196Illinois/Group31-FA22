456c1997f7070df21403e2ac67693871c3cc87eb
rohannunu <rohann5@illinois.edu>
2022-12-02 15:00:02 -0600

    Experimenting with NavBar front end changes

--- Project/CV_WebApp_Implementation/main.py
+++ Project/CV_WebApp_Implementation/main.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('testDisplay.html')
 
+@app.route('/bicep_curls')
+def bicep_curls():
+    return render_template('BicepCurls.html')
+
+@app.route('/jumping_jacks')
+def jumping_jacks():
+    return render_template('JumpingJacks.html')
+
+@app.route('/toe_touch')
+def toe_touch():
+    return render_template('ToeTouch.html')
+
+@app.route('/stats')
+def stats_page():
+    return render_template("Stats.html")
+
 def gen(camera):
     while camera.video.isOpened():
         frame = camera.get_frame()
