bf70fae3f973b9151e783c077d35b819f80edc2d
rohannunu <rohann5@illinois.edu>
2022-12-03 01:18:01 -0600

    Integrated Frontend to Backend

--- Project/CV_WebApp_Implementation/main.py
+++ Project/CV_WebApp_Implementation/main.py
@@ -4,8 +4,24 @@ from camera import VideoCamera
 app = Flask(__name__)
 
 @app.route('/')
-def index(VideoCamera):
-    return render_template('testDisplay.html', count= VideoCamera.getCount())
+def index():
+    return render_template('testDisplay.html')
+
+@app.route('/bicep_curls')
+def bicep_curls():
+    return render_template('BicepCurls.html')
+
+@app.route('/jumping_jacks')
+def jumping_jacks():
+    return render_template('JumpingJacks.html')
+
+@app.route('/toe_touch')
+def toe_touch():
+    return render_template('ToeTouch.html')
+
+@app.route('/stats')
+def stats():
+    return render_template('Stats.html')
 
 def gen(camera):
     while camera.video.isOpened():
